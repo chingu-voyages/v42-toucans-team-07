d4e8331c6c3feb12e37f351b465fb1381523dd95
Petre4488 <44945573+Petre4488@users.noreply.github.com>
2023-01-24 18:22:30 +0200

    update homepage & navbar

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-const toggle = document.querySelector(".toggle-item"),
+const toggle = document.querySelector("#darkMode"),
       body=document.querySelector('body'),
       context=document.getElementById('context');
-
+      authBtn=document.getElementById('loginBtn');
+      dropDown=document.getElementById('navbar')
 
       toggle.addEventListener("click", function(){
         this.classList.toggle("active");
         body.classList.toggle("active");
+        authBtn.classList.toggle('active');
+        dropDown.classList.toggle('active');
 
         // function to change content from light -- dark
 
         if(toggle.classList.contains("active")){
-            context.innerHTML="Light Mode";
-            context.style.color="#D3D3D3";
+            element = document.getElementById('darkMode').id = "darkMode"
         }else{
-            context.innerHTML="Dark Mode";
-            context.style.color= "#212529";
         }
       });
