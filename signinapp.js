0998e5c6659e85534df5f8ce0b8ed7dcafe5079f
hanna <hannawkozak@gmail.com>
2023-02-08 21:16:10 +0000

    feat: attached signin form to home page

--- signinapp.js
+++ signinapp.js
@@ -1,7 +1,22 @@
 let btn = document.querySelector(".signUp");
-
+const signInForm = document.querySelector(".sign-in");
+const message = document.querySelector(".message");
 
 btn.addEventListener("click", function (event) {
-    event.preventDefault();
-    window.location.href = "signup.html";
+  event.preventDefault();
+  window.location.href = "signup.html";
 });
+
+signInForm.addEventListener("submit", (event) => {
+  message.innerHTML =
+    "Login was successful!  You are being redirected to home page!";
+  message.style.color = "#2453c8";
+  message.style.textAlign = "center";
+  message.style.fontSize = "1.6rem";
+  setTimeout(redirectToHomePage, 2000);
+  event.preventDefault();
+});
+
+const redirectToHomePage = () => {
+  window.location.href = "./homePage.html";
+};
--- signup.js
+++ signup.js
@@ -1,7 +1,22 @@
 let signin = document.querySelector(".signIn");
-
+const signUpForm = document.querySelector(".sign-in");
+const message = document.querySelector(".message");
 
 signin.addEventListener("click", function (event) {
-    event.preventDefault();
-    window.location.href = "signin.html";
+  event.preventDefault();
+  window.location.href = "signin.html";
 });
+
+signUpForm.addEventListener("submit", (event) => {
+  message.innerHTML =
+    "Signup was successful! You are being redirected to home page!";
+  message.style.color = "#2453c8";
+  message.style.textAlign = "center";
+  message.style.fontSize = "1.6rem";
+  setTimeout(redirectToHomePage, 2000);
+  event.preventDefault();
+});
+
+const redirectToHomePage = () => {
+  window.location.href = "./homePage.html";
+};
